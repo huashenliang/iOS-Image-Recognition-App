a0f8ca99f1a91302621fce0e8cedbaaacc386097
Huashen Liang <JohnnyLiang77@gmail.com>
2019-06-02 21:15:34 -0400

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by huashen liang on 2019-06-02.
+//  Copyright © 2019 huashen liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
